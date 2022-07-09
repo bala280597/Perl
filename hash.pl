b689b9e412a058abe50516a67d7c9de37ae03ecb
Balamurugan <47313756+bala280597@users.noreply.github.com>
2022-07-09 17:29:16 +0530

    Update hash.pl

--- hash.pl
+++ hash.pl
@@ -11,18 +11,31 @@ sub main {
 	"May" => 5,
 	"July" => 7,
   );
-  print $months{'May'}."\n";
+  #print $months{'May'}."\n";
   
-  my @months = keys %months;
+  #my @months = keys %months;
   
   #for my $month(@months){
   # my $value = $months{$month};
 	#print "$value\n";
   #}
   
-  while(my ($key,$value) = each %months ) {
-    print "$key : $value\n "
+  #while(my ($key,$value) = each %months ) {
+  #  print "$key : $value\n "
+  #}
   
+  my %foods = ('milk' => 'cat', 'Bone'=> 'Dog','birds' => 'seeds');
+  
+  #while ( my ($key,$value) = each %foods ){
+  #   print "$key : $value \n"  
+  #}
+  
+  #my @animals = keys %foods;
+  
+  foreach my $key(sort keys %foods) {
+    my $value = $foods{$key};
+	print "$key = $value \n";
+	
   }
 }
 
